d626da624c7266848778111264a64aefe12a9410
maplesama <maplesyrup0819@gmail.com>
2025-10-23 12:55:01 +0900

    top-page create

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JevilController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//以下記載
+Route::get('/toppage', [JevilController::class, 'index']);
